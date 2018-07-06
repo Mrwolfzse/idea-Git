34afe57e19c9594efcd7189ec13143be43d0b150
Mr_WOLF <861634943@qq.com>
2018-07-06 20:00:48 +0800

    idea提交代码到本地服务器

---
+++ src/com/git/git.java
@@ -0,0 +1,9 @@
+package com.git;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("world1");
+        System.out.println("world2");
+        System.out.println("world3");
+    }
+}
